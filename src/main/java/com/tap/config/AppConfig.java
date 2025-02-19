b7794ce840a3f6f6e3b26e24d92b79e294e3f8fd
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2025-02-19 10:33:03 +0530

    Update AppConfig.java

--- src/main/java/com/tap/config/AppConfig.java
+++ src/main/java/com/tap/config/AppConfig.java
@@ -10,19 +10,21 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+//the explicit configuration statement like Configuration configuration = new Configuration().configure(); 
+//88is not needed because Spring handles it implicitly when it processes your AppConfig class
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "com.tap")
 public class AppConfig {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+    
 
+    //The DataSource is used by Hibernate to connect to the database.
     @Bean
     public DataSource dataSource() {
-        LOGGER.info("Creating DataSource bean");
+       
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/onlinebanking");
@@ -31,16 +33,20 @@ public class AppConfig {
         return dataSource;
     }
 
+  //The LocalSessionFactoryBean is a Spring-provided factory bean that creates a Hibernate SessionFactory.
+  //sessionFactory.setPackagesToScan("com.tap.model");, Spring will automatically scan the specified package (in this case, com.tap.model) for all annotated entity classes.
+   //This means you do not need to individually add each annotated class using methods like cfg.addAnnotatedClass(Customer.class);.
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
-        LOGGER.info("Creating SessionFactory bean");
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean(); 
         sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.tap.model");
+        sessionFactory.setPackagesToScan("com.tap.model"); 
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
 
+    //This method creates a Properties object and sets Hibernate properties
     private Properties hibernateProperties() {
         Properties properties = new Properties();
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
@@ -49,9 +55,10 @@ public class AppConfig {
         return properties;
     }
 
+    //This bean is crucial for managing transactions in your Hibernate-based application
     @Bean
     public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        LOGGER.info("Creating TransactionManager bean");
+       
         HibernateTransactionManager txManager = new HibernateTransactionManager();
         txManager.setSessionFactory(sessionFactory);
         return txManager;
