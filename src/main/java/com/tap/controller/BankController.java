7c1fd5d6711fa3b3867c297e93d55353111a6297
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2025-03-04 19:37:50 +0530

    BankController.java

--- src/main/java/com/tap/controller/BankController.java
+++ src/main/java/com/tap/controller/BankController.java
@@ -1,5 +1,5 @@
-package com.tap.controller;
 
+package com.tap.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,11 +8,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.tap.dao.AccountDAO;
-import com.tap.dao.UserDAO;
 import com.tap.model.Account;
 import com.tap.model.User;
+import com.tap.service.AccountService;
+import com.tap.service.UserService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -20,51 +19,44 @@ import jakarta.persistence.PersistenceContext;
 public class BankController {
 
     @Autowired
-    private UserDAO userDAO;
+    private UserService userService;
 
     @Autowired
-    private AccountDAO accountDAO;
-    
+    private AccountService accountService;
+
     @PersistenceContext
     private EntityManager entityManager;
 
     @RequestMapping("/")
     public String showHomePage(Model model) {
-        System.out.println("Entering home");
         model.addAttribute("message", "Welcome to Online Banking!");
         return "home"; // Loads home.jsp
     }
 
     @GetMapping("/login")
     public String showLoginPage() {
-        System.out.println("Inside showLoginPage method - Login page requested");
         return "login";
     }
 
     @PostMapping("/bankLogin")
     public String login(@RequestParam String email, @RequestParam String password, Model model) {
-        System.out.println("Logging in User: " + email);
         model.addAttribute("user", email);
         return "dashboard"; // Change to your actual dashboard view
     }
 
     @GetMapping("/register")
     public String showRegisterPage() {
-        System.out.println("Inside showRegisterPage method - Register page requested");
         return "register";
     }
 
-    
-
     @GetMapping("/BankAccount")
     public String showAccountPage() {
-        System.out.println("Inside showRegisterPage method - Register page requested");
         return "account";
     }
 
     @PostMapping("/account")
     public String createAccount(@ModelAttribute Account account, Model model) {
-        accountDAO.saveAccount(account);
+        accountService.saveAccount(account);
         model.addAttribute("account", account);
         return "dashboard"; // Redirects to the success page
     }
