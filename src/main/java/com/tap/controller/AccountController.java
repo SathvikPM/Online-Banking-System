68365f6105cd721e389078c1fe84d5d647193013
Sathvik PM <SATHVIKPAAROL2002@GMAIL.COM>
2025-03-04 19:37:15 +0530

    AccountController.java

--- src/main/java/com/tap/controller/AccountController.java
+++ src/main/java/com/tap/controller/AccountController.java
@@ -1,3 +1,4 @@
+
 package com.tap.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,58 +6,50 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.tap.dao.AccountDAO;
-import com.tap.dao.TransactionDAO;
 import com.tap.model.Account;
 import com.tap.model.Transaction;
 import com.tap.model.User;
-
+import com.tap.service.AccountService;
+import com.tap.service.TransactionService;
 import jakarta.servlet.http.HttpSession;
-
 import java.util.List;
-import java.util.logging.Logger;
 
 @Controller
 public class AccountController {
 
-  
+    @Autowired
+    private AccountService accountService;
 
     @Autowired
-    private AccountDAO accountDAO;
-
-    @Autowired
-    private TransactionDAO transactionDAO;
+    private TransactionService transactionService;
 
     @GetMapping("/viewAccounts")
     public String viewAccounts(HttpSession session, Model model) {
-        // Ensure user is logged in
         User user = (User) session.getAttribute("user");
         if (user == null) {
-            return "redirect:/userLogin"; // Redirect to login page if user is not logged in
+            return "redirect:/userLogin";
         }
 
         int userId = user.getUserId();
-      List<Account> accounts = accountDAO.getAccountsByUserId(userId);
+        List<Account> accounts = accountService.getAccountsByUserId(userId);
         model.addAttribute("accounts", accounts);
 
-        return "viewAccounts"; // Loads viewAccounts.jsp
+        return "viewAccounts";
     }
 
     @GetMapping("/viewAccountTransactions")
     public String viewAccountTransactions(@RequestParam int accountId, HttpSession session, Model model) {
-        // Ensure user is logged in
         User user = (User) session.getAttribute("user");
         if (user == null) {
-            return "redirect:/userLogin"; // Redirect to login page if user is not logged in
+            return "redirect:/userLogin";
         }
 
-        List<Transaction> transactions = transactionDAO.getTransactionsByAccountId(accountId);
+        List<Transaction> transactions = transactionService.getTransactionsByAccountId(accountId);
         model.addAttribute("transactions", transactions);
-        model.addAttribute("accountId", accountId); // Add accountId to model for displaying in the view
-        return "viewTransactions"; // Loads viewTransactions.jsp
+        model.addAttribute("accountId", accountId);
+        return "viewTransactions";
     }
-    
+
     @GetMapping("/welcome")
     public String welcomeFile() {
         return "welcome";
